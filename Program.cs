cf677511c82d646e5e123bd269429f14d5a05abb
motoi-tsushima <motoi.tsushima@gmail.com>
2021-02-22 17:04:12 +0900

    ヘルプ表示。 テスト用のバイナリ・ダミーファイル作成ツール。 サイズを自由に指定できる。

--- Program.cs
+++ Program.cs
@@ -6,6 +6,21 @@ namespace dummyfile
     {
         static void Main(string[] args)
         {
+            string[] help = { 
+                "" 
+                , "dummyfile v1.0.0.0  Copyright 2021 motoi.tsushima."
+                , ""
+                , " options."
+                , "     /s:filesize     ファイルサイズ(byte size)を指定する。(単位として、k,m,g,tを指定可能)"
+                , "     /d              ダミーデータを書き込む。(0x01から0xFFまでの数値を、繰り返し書き込む)"
+                , ""
+            };
+
+            foreach(var row in help)
+            {
+                Console.WriteLine(row);
+            }
+
             Options options = new Options(args);
 
             CreateDummyFile createDummyFile = new CreateDummyFile(options.FileName, options.FileSize);
@@ -14,7 +29,7 @@ namespace dummyfile
 
             createDummyFile.Create();
 
-            Console.WriteLine("CreateDummyFile {0}, {1} !", options.FileName, options.FileSize);
+            Console.WriteLine("CreateDummyFile {0}, {1}byte !", options.FileName, options.FileSize);
         }
     }
 }
